8814a966ed8eef687bef514d7dd56bf87159918e
MURILO FRANCISCO MATOS <CG3028151@car.ifsp>
2025-04-02 20:21:54 -0300

    Fix: button sizes, href and hover

--- src/app/(public)/initial/page.tsx
+++ src/app/(public)/initial/page.tsx
@@ -5,27 +5,29 @@ import Link from "next/link";
 function page() {
   return (
     <div className="flex h-screen">
-      <div className="m-auto px-6 py-12 md:border-2 md:rounded-4xl flex flex-col justify-center items-center">
+      <div className="m-auto px-6 py-12 md:border-2 md:rounded-xl flex flex-col justify-center items-center">
         <Image
           src="Criarte.svg"
           alt="Criart"
-          width={125}
-          height={125}
-          className="mb-4"
+          width={110}
+          height={110}
+          className="mb-8"
         ></Image>
-        <h1 className="font-bold text-7xl my-12">Olá</h1>
-        <Button className="font-semibold bg-[#FF9D00] inset-shadow-sm text-bla inset-shadow-black/25 text-2xl py-8 px-8 rounded-3xl">
-          Ja possuo Cadastro
-        </Button>
-        <Button className="mt-5 font-semibold bg-[#ABCFB5] inset-shadow-sm text-bla inset-shadow-black/25 text-2xl py-8 px-8 rounded-3xl">
-          Quero me Cadastrar
-        </Button>
+        <h1 className="font-bold text-6xl my-12">Olá!</h1>
+        <Link href={"/sign-in"}>
+          <Button className="font-semibold cursor-pointer hover:bg-[#FF9D00]/70 bg-[#FF9D00] inset-shadow-sm text-bla inset-shadow-black/40 text-xl py-7 px-7 rounded-3xl">
+            Ja possuo Cadastro
+          </Button>
+        </Link>
+        <Link href={"/sign-up"}>
+          <Button className="mt-5 font-semibold cursor-pointer hover:bg-[#ABCFB5]/70 bg-[#ABCFB5] inset-shadow-sm text-bla inset-shadow-black/40 text-xl py-7 px-7 rounded-3xl">
+            Quero me Cadastrar
+          </Button>
+        </Link>
         <Link href={"/initial"} className="text-md font-light underline mt-6">
-          {" "}
           Continuar sem login
         </Link>
         <Link href={"/initial"} className="text-xl italic underline mt-7">
-          {" "}
           Sou artista
         </Link>
       </div>
