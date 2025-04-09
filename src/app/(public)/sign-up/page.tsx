d938ea203e7af041cea6a178e618bc8aec49630a
SamuelRSADS <samueluba08@gmail.com>
2025-04-08 22:08:30 -0300

    refactor: input

--- src/app/(public)/sign-up/page.tsx
+++ src/app/(public)/sign-up/page.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { FiChevronLeft } from "react-icons/fi";
+import SignInput from "./components/SignInput";
 import Link from 'next/link'
 
 function SignUp() {
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="rounded-lg shadow-md p-[44px] min-h-[852px] min-w-[393px] border-1">
-            <header>
-                <Link href="/initial"><FiChevronLeft className="h-12 w-12"/></Link>
+            <header className="mb-6">
+              <Link href="/initial"><FiChevronLeft className="" size={28}/></Link>
             </header>
     
         <div className="mb-6">
-          <h1 className="text-6xl md:text-6xl font-bold mb-2">Olá!</h1>
+          <h1 className="text-5xl md:text-5xl font-bold mb-2">Olá!</h1>
           <p>
             Cadastre-se e <br />descubra o melhor do artesanato.
           </p>
         </div>
         <div>
             <form className="space-y-3 mb-6 text-[#985E00]">
-                <Input placeholder="Nome" type="text" className="w-full h-12 bg-[#FFF2DE] placeholder:text-[#985E00] rounded-[20px] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] border border-none font-bold" />
-                <Input placeholder="Nome Social" type="text" className="w-full h-12 bg-[#FFF2DE] placeholder:text-[#985E00] rounded-[20px] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] border border-none font-bold" />
-                <Input placeholder="CPF" type="text" className="w-full h-12 bg-[#FFF2DE] placeholder:text-[#985E00] rounded-[20px] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] border border-none font-bold" />
-                <Input placeholder="Email" type="email" className="w-full h-12 bg-[#FFF2DE] placeholder:text-[#985E00] rounded-[20px] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] border border-none font-bold" />
-                <Input placeholder="Data de Nascimento" type="date" className="w-full  h-12 bg-[#FFF2DE] placeholder:text-[#985E00] rounded-[20px] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] border border-none font-bold" />
-                <Input placeholder="Senha" type="password" className="w-full h-12 bg-[#FFF2DE] placeholder:text-[#985E00] rounded-[20px] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] border border-none font-bold" />
-                <Input placeholder="Endereco" type="text" className="w-full h-12 bg-[#FFF2DE] placeholder:text-[#985E00] rounded-[20px] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] border border-none font-bold" />
-                <Input placeholder="Telefone" type="tel" className="w-full h-12 bg-[#FFF2DE] placeholder:text-[#985E00] rounded-[20px] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] border border-none font-bold" />
+                <SignInput placeholder={"Nome"} type={"text"}/>
+                <SignInput placeholder={"Nome Social"} type={"text"}/>
+                <SignInput placeholder={"CPF"} type={"text"}/>
+                <SignInput placeholder={"Email"} type={"email"}/>
+                <SignInput placeholder={"Data de Aniversario"} type={"date"}/>
+                <SignInput placeholder={"Senha"} type={"password"}/>
+                <SignInput placeholder={"Confirmação de Senha"} type={"password"}/>
+                <SignInput placeholder={"Endereço"} type={"text"}/>
+                <SignInput placeholder={"Telefone"} type={"tel"}/>
             </form>
         </div>
 
