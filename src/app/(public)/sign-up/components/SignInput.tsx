6f406afbf41c96e4b505e6ba7c232cba74f8885b
SamuelRSADS <samueluba08@gmail.com>
2025-04-20 22:10:38 -0300

    refactor(auth): Melhora página de cadastro com integração API e UX
    - Implementa estrutura de chamada API na função createUser, preparando dados do usuário (incluindo número de telefone combinado) para envio (endpoint de placeholder).
    - Adiciona tratamento básico de sucesso/erro com alertas para o processo de criação de usuário.
    - Introduz estado isLoading para desabilitar o botão de envio durante a submissão do formulário.
    - Implementa auto-ocultação para o banner de erro geral do formulário após 5 segundos usando setTimeout.
    - Adiciona bloco 	ry...catch para acesso ao sessionStorage no fluxo de artesão para maior robustez.
    - Implementa funcionalidade de clique fora para o popover de informações 'Sou Artesão' para melhorar a usabilidade.

--- src/app/(public)/sign-up/components/SignInput.tsx
+++ src/app/(public)/sign-up/components/SignInput.tsx
@@ -1,17 +1,12 @@
 import { forwardRef } from "react";
 import { Input } from "@/components/ui/input";
 
-const inputStyles = {
-  WebkitTextFillColor: "#985E00",
-  transition: "background-color 5000s ease-in-out 0s",
-};
-
 const SignInput = forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement> & { hasError?: boolean }>(
   ({hasError, className, ...props}, ref) => (
     <Input
+      
       ref={ref}
-      className={`${className} ${hasError ? 'border-red-500 placeholder:text-magenta' : 'border-none placeholder:text-[#985E00]'} w-full h-10 bg-[#FFF2DE]  rounded-[20px] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] font-bold`}
-      style={inputStyles}
+      className={`${className} ${hasError ? 'border-magenta border-2 focus-visible:ring-2 focus-visible:border-magenta placeholder:text-magenta' : 'border-none placeholder:text-[#985E00]'} w-full h-10 bg-[#FFF2DE]  rounded-[20px] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] font-bold`}
       {...props}
     />
   )
--- src/app/(public)/sign-up/page.tsx
+++ src/app/(public)/sign-up/page.tsx
@@ -1,19 +1,22 @@
 'use client'
 
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Button } from "@/components/ui/button";
 import { FiChevronLeft } from "react-icons/fi";
 import SignInput from "./components/SignInput";
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
+import { AiOutlineEye, AiOutlineEyeInvisible,  AiOutlineInfoCircle } from "react-icons/ai";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 const signUpSchema = z.object({
-  nome: z.string()
+  name: z.string()
     .min(2, { message: "Digite um nome com pelo menos 2 letras." }),
 
-  nomeSocial: z.string().optional(),
+  socialName: z.string().optional(),
 
   cpf: z.string()
     .min(11, { message: "O CPF deve ter 11 números." })
@@ -24,11 +27,32 @@ const signUpSchema = z.object({
     .nonempty({ message: "O e-mail é obrigatório." })
     .email({ message: "Digite um e-mail válido." }),
 
-  dataAniversario: z.string()
-    .nonempty({ message: "A data de nascimento é obrigatória." }),
+  birthDate: z.string()
+    .nonempty({ message: "A data de nascimento é obrigatória." })
+    .refine(date => {
+      const birth = new Date(date);
+      return !isNaN(birth.getTime()) && birth < new Date();
+    }, { message: "Digite uma data válida no passado." })
+    .refine(date => {
+      const birth = new Date(date);
+      const today = new Date();
+      const eighteenYearsAgo = new Date(
+        today.getFullYear() - 18,
+        today.getMonth(),
+        today.getDate()
+      );
+      return birth <= eighteenYearsAgo;
+    }, { message: "Você deve ter pelo menos 18 anos." }),
 
-  senha: z.string()
-    .min(8, { message: "A senha deve ter no mínimo 8 caracteres." }),
+  password: z
+    .string()
+    .min(8, 'Senha Inválida - minimo de 8')
+    .regex(/[!@#$%*&^(),.?":{}|<>]/, 'Senha Inválida - caractere especial')
+    .regex(/[A-Z]/, 'Senha Inválida - letra maiuscula')
+    .regex(/[a-z]/, 'Senha Inválida - letra minuscula')
+    .regex(/[0-9]/, 'Senha Inválida - numero'),
+
+  confirmPassword: z.string(),
 
   codigoPais: z.string()
     .nonempty({ message: "O código do país é obrigatório." }),
@@ -39,43 +63,85 @@ const signUpSchema = z.object({
     .max(2, { message: "O código de área deve ter 2 dígitos." })
     .regex(/^\d{2}$/, { message: "O código de área deve conter apenas números." }),
   
+  phone: z.string()
+    .min(8, { message: "O telefone deve ter no mínimo 8 dígitos." })
+    .max(9, { message: "O telefone deve ter no máximo 9 dígitos." })
+    .regex(/^\d{8,9}$/, {message: "O telefone deve possuir apenas numeros"}),
 
-  telefone: z.string()
-    .min(10, { message: "O telefone deve ter no mínimo 10 dígitos (DDD + número)." })
-    .max(11, { message: "O telefone deve ter no máximo 11 dígitos." })
-    .regex(/^\d{10,11}$/, {message: "O telefone deve possuir apenas numeros"}),
-});
+  isArtisan: z.boolean().optional(),
 
+}).refine((fields) => fields.password === fields.confirmPassword,{
+    path: ['confirmPassword'],
+    message: 'As senhas precisam ser iguais'
+})
 
 type signUpSchemaData = z.infer<typeof signUpSchema>
 
 function SignUp() {
+  const router = useRouter(); 
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [showPasswordInfo, setShowPasswordInfo] = useState(false)
+  const [hasFormError, setHasFormError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const timeoutRef = useRef<number | null>(null);
+
+  const [showPopover, setShowPopover] = useState(false);
+  const popoverRef = useRef<HTMLDivElement>(null);
+
   const {register,
     handleSubmit, 
     formState: {errors}} 
     = useForm<signUpSchemaData>({
       resolver: zodResolver(signUpSchema),
       defaultValues: {
-        nome: "",
-        nomeSocial: "",
+        name: "",
+        socialName: "",
         cpf: "",
         email: "",
-        dataAniversario: "",
-        senha: "",
+        birthDate: "",
+        password: "",
         codigoPais: "+55",
         ddd: "",
-        telefone: ""
+        phone: "",
+        isArtisan: false,
       },
   });
 
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (popoverRef.current && !popoverRef.current.contains(event.target as Node)) {
+        setShowPopover(false);
+      }
+    }
+
+    if (showPopover) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [showPopover]);
+
   async function createUser(data: signUpSchemaData) {
-    const telefoneCompleto = `${data.codigoPais}${data.ddd}${data.telefone}`;
+    const telefoneCompleto = `${data.codigoPais}${data.ddd}${data.phone}`;
   
     const userData = {
-      ...data,
-      telefone: telefoneCompleto,
+      name: data.name,
+      socialName: data.socialName,
+      cpf: data.cpf,
+      email: data.email,
+      password: data.password,
+      birthDate: data.birthDate,
+      phone: telefoneCompleto,
     };
-  
+    
+    console.log(userData)
+
+
     try {
       const response = await fetch("", {
         method: "POST",
@@ -84,26 +150,70 @@ function SignUp() {
         },
         body: JSON.stringify(userData),
       });
-  
+
       if (!response.ok) {
         throw new Error("Erro ao criar usuário.");
-      }
+      } 
   
       alert("Usuário criado com sucesso!");
+      router.push('/sign-in');
     } catch (error) {
       console.error("Erro:", error);
-      alert("Erro ao criar usuário. Tente novamente.");
+      const errorMessage = error instanceof Error ? error.message : "Erro ao criar usuário. Tente novamente.";
+      alert(errorMessage);
     }
   }
+
+  const handleFormSubmit = (data: signUpSchemaData) => {
+    setHasFormError(false); 
+
+    if (data.isArtisan) {
+      console.log("Usuário é artesão, redirecionando...");
+
+      try{
+        sessionStorage.setItem('artisanRegStep1Data', JSON.stringify(data));
+        router.push('/initial');
+      } catch (error){
+        console.error("Erro ao salvar dados no sessionStorage:", error);
+        alert("Ocorreu um erro inesperado. Por favor, tente novamente.");
+      }
+    } else {
+      console.log("Usuário não é artesão, criando usuário comum...");
+      createUser(data);
+    }
+  };
+
+  const handleFormError = (formErrors: any) => {
+      console.log("Erros no formulário:", formErrors)
+      if (Object.keys(formErrors).length > 0) {
+        setHasFormError(true);
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+        timeoutRef.current = window.setTimeout(() => {
+          setHasFormError(false);
+        }, 5000);
+      }
+    }
   
   return (
 
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="rounded-lg shadow-md p-[44px] min-h-[852px] sm:w-96  w-full border-1">
             <header className="mb-6">
-              <Link href="/initial"><FiChevronLeft className="" size={28}/></Link>
+
+            {hasFormError ? (
+            <div className="flex items-center justify-center text-white bg-magenta rounded-3xl text-xs p-2.5 font-bold w-full">
+              <FaExclamationTriangle className="mr-1" />
+              <span>Campo obrigatório ou com formato inválido.</span>
+            </div>
+          ) : (
+            <Link href="/initial">
+              <FiChevronLeft size={28} />
+            </Link>
+          )}
             </header>
-    
+
         <div className="mb-6">
           <h1 className="text-5xl md:text-5xl font-bold mb-2">Olá!</h1>
           <p>
@@ -111,14 +221,14 @@ function SignUp() {
           </p>
         </div>
         <div>
-            <form className="space-y-3 mb-6 text-[#985E00]" onSubmit={handleSubmit(createUser)}>
+            <form className="space-y-3 mb-6 text-[#985E00]" onSubmit={handleSubmit(handleFormSubmit, handleFormError)}>
               <div>
-                <SignInput placeholder={"Nome*"} type={"text"} {...register('nome')} hasError={!!errors.nome}/>
-                {errors.nome && <span className='text-sm text-red-500'>{errors.nome.message}</span>}
+                <SignInput placeholder={"Nome*"} type={"text"} {...register('name')} hasError={!!errors.name}/>
+                {errors.name && <span className='text-sm text-red-500'>{errors.name.message}</span>}
               </div>
 
               <div>
-                <SignInput placeholder={"Nome Social"} type={"text"} {...register('nomeSocial')}/>
+                <SignInput placeholder={"Nome Social"} type={"text"} {...register('socialName')}/>
               </div>
 
               <div>
@@ -132,39 +242,132 @@ function SignUp() {
               </div>
 
               <div>
-                <SignInput placeholder={"Data de Nascimento"} type={"date"} {...register('dataAniversario')} hasError={!!errors.dataAniversario}/>
-                {errors.dataAniversario && <span className='text-sm text-red-500'>{errors.dataAniversario.message}</span>}
+                <SignInput placeholder={"Data de Nascimento"} type={"date"} {...register('birthDate')} hasError={!!errors.birthDate}/>
+                {errors.birthDate && <span className='text-sm text-red-500'>{errors.birthDate.message}</span>}
               </div>
 
-              <div>
-                <SignInput placeholder={"Senha*"} type={"password"} {...register('senha')} hasError={!!errors.senha}/>
-                {errors.senha && <span className='text-sm text-red-500'>{errors.senha.message}</span>}
+              <div className="w-full">
+                <div className="relative">
+                  <SignInput
+                    placeholder="Senha*"
+                    type={showPassword ? "text" : "password"}
+                    {...register('password')}
+                    hasError={!!errors.password}
+                    onFocus={() => setShowPasswordInfo(true)}
+                    onBlur={() => setShowPasswordInfo(false)}
+                  />
+                  <button
+                    aria-label="Mostrar ou esconder senha"
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#985E00]"
+                  >
+                    {showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+                  </button>
+                </div>
+
+                {errors.password && (
+                  <span className="text-sm text-red-500">{errors.password.message}</span>
+                )}
+
+                {showPasswordInfo && !errors.password && (
+                  <div className="text-xs text-[#985E00] p-2 rounded">
+                    <p>• No mínimo 8 caracteres</p>
+                    <p>• Pelo menos 1 letra maiúscula</p>
+                    <p>• Pelo menos 1 letra minúscula</p>
+                    <p>• Pelo menos 1 número</p>
+                    <p>• Pelo menos 1 caractere especial (como !, @, #, etc.)</p>
+                  </div>
+                )}
               </div>
 
-              <div>
-                <SignInput placeholder={"Confirmação de Senha*"} type={"password"} />
+
+              <div className="w-full">
+                <div className="relative">
+                  <SignInput
+                    placeholder={"Confirmação de Senha*"}
+                    type={showConfirmPassword ? "text" : "password"}
+                    {...register('confirmPassword')}
+                    hasError={!!errors.confirmPassword}
+                  />
+                  <button
+                    aria-label="Mostrar ou esconder senha"
+                    type="button"
+                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#985E00]"
+                  >
+                    {showConfirmPassword ? <AiOutlineEye/> : <AiOutlineEyeInvisible />}
+                  </button>
+                </div>
+                {errors.confirmPassword && (
+                  <span className="text-sm text-red-500">{errors.confirmPassword.message}</span>
+                )}
               </div>
 
               <div className='grid grid-cols-4 gap-2'>
-                  <select {...register('codigoPais')} className="p-2 border bg-[#FFF2DE] placeholder:text-[#985E00] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] rounded-[20px] font-bold ">
+                  <select {...register('codigoPais')} className="p-2 border bg-[#FFF2DE] shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)] rounded-[20px] font-bold ">
                     <option value="+55">+55</option>
                   </select>
 
-                <div>
+                <div className='text-center'>
                   <SignInput placeholder={"DDD"} type={"text"} {...register('ddd')} hasError={!!errors.ddd}/>
                 </div>
 
                 <div className='col-span-2'>
-                  <SignInput placeholder={"Telefone"} type={"tel"} {...register('telefone')} hasError={!!errors.telefone}/>
+                  <SignInput placeholder={"Telefone"} type={"tel"} {...register('phone')} hasError={!!errors.phone}/>
                 </div>
               </div>
 
-              {(errors.telefone || errors.ddd || errors.codigoPais) && (
+              {(errors.phone || errors.ddd || errors.codigoPais) && (
                 <p className='text-sm text-red-500'>
-                  {errors.telefone?.message || errors.ddd?.message || errors.codigoPais?.message}
+                  {errors.phone?.message || errors.ddd?.message || errors.codigoPais?.message}
                 </p>
               )}
+              
+              <div className="flex items-center justify-between bg-[#F6F6F6] text-mint-600 w-full h-10 rounded-3xl py-1 px-3 shadow-[inset_0_2px_6px_rgba(0,0,0,0.4)]">
+                <div className="flex items-center">
+                  <input
+                    id="isArtisan"
+                    type="checkbox"
+                    {...register('isArtisan')}
+                    className="ml-2 mr-2 h-5 w-3 text-[#E05D00]"
+                  />
+                  <label htmlFor="isArtisan" className="text-base">
+                    Sou Artesão
+                  </label>
+                </div>
+                <button type="button"
+                    onClick={() => setShowPopover(!showPopover)}
+                    className="relative">
+                  <AiOutlineInfoCircle className="text-black" />
+                </button>
 
+                {showPopover && (
+                  <>
+                    <div
+                      className="fixed inset-0 bg-black/25 z-40"
+                    ></div>
+
+                    <div className="fixed inset-0 flex items-center justify-center z-50">
+                        <div
+                            ref={popoverRef}
+                            className="bg-white text-black text-sm rounded-3xl shadow-lg p-5 text-center max-w-72 h-auto"
+                          >
+                            <div className='flex justify-end mb-1'>
+                              <button onClick={() => setShowPopover(false)} className='font-sm'>X</button>
+                            </div>
+                            <p className="font-bold mb-2">Artesão credenciado</p>
+                            <p className="text-xs">
+                              Marque essa opção se você é artesão(a) cadastrado e faz parte da associação vinculada à <strong>Fundacc</strong> (Fundação Educacional e Cultural de Caraguatatuba). Isso garante acesso a funcionalidades exclusivas para membros reconhecidos.
+                            </p>
+                        </div>
+                    </div>
+                  
+                  </>
+
+                )}
+                
+              </div>
 
               <div className="text-sm text-gray-600 text-center mb-6">
                 <p>
@@ -176,8 +379,8 @@ function SignUp() {
               </div>
 
               <div className="flex justify-center">
-                  <Button className="w-[191px] h-[42px] bg-[#E05D00] hover:bg-[#c05000] text-white py-2 px-6 rounded-[20px] border-b-4 border-[#a04500] " type='submit'>
-                      Continuar
+                  <Button disabled={isLoading} className="w-[191px] h-[42px] bg-[#E05D00] hover:bg-[#c05000] text-white py-2 px-6 rounded-[20px] border-b-4 border-[#a04500] " type='submit'>
+                    {isLoading ? 'Enviando...' : 'Continuar'}
                   </Button>
               </div>
             </form>
