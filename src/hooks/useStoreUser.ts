d666a47edab900dcfc5b661f834c9cfa4f2ce39a
MARCUS VINICIUS DA CRUZ SANTOS <CG3028518@car.ifsp>
2025-05-07 19:41:22 -0300

    feat: adding user data persistancy when the page is reloads

--- src/hooks/useStoreUser.ts
+++ src/hooks/useStoreUser.ts
@@ -1,28 +1,36 @@
 import { create } from "zustand";
 import { UserProps, UserStore } from "@/types/UserProps";
+import { persist } from "zustand/middleware";
 
-const useStoreUser = create<UserStore>((set) => ({
-  user: {
-    userName: "",
-    userPhoto: "",
-    isAuthenticated: false,
-  },
-  setUser: (user: UserProps) =>
-    set(() => ({
-      user: {
-        userName: user.userName,
-        userPhoto: user.userPhoto,
-        isAuthenticated: true,
-      },
-    })),
-  resetStore: () =>
-    set(() => ({
+const useStoreUser = create<UserStore>()(
+  persist(
+    (set) => ({
       user: {
         userName: "",
         userPhoto: "",
         isAuthenticated: false,
       },
-    })),
-}));
+      setUser: (user: UserProps) =>
+        set(() => ({
+          user: {
+            userName: user.userName,
+            userPhoto: user.userPhoto,
+            isAuthenticated: true,
+          },
+        })),
+      resetStore: () =>
+        set(() => ({
+          user: {
+            userName: "",
+            userPhoto: "",
+            isAuthenticated: false,
+          },
+        })),
+    }),
+    {
+      name: "loginStore",
+    }
+  )
+);
 
 export default useStoreUser;
