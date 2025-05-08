6112a6e665cf746941f7f1861c6073f98270306e
Murilo Francisco Matos1 <murilofm.social@gmail.com>
2025-05-08 14:24:35 -0300

    Fix: spacing and centralization at 'O que você Procura?'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex flex-col items-center justify-items-center min-h-screen px-4 sm:px-12 lg:px-54 pb-20 gap-16 py-8 font-[family-name:var(--font-poppins)]">
-        <h2 className="text-3xl font-bold mb-[-32px]">O que você procura?</h2>
+      <main className="flex flex-col items-center justify-items-center min-h-screen px-4 sm:px-12 lg:px-54 pb-20 gap-10 py-8 font-[family-name:var(--font-poppins)]">
+        <h2 className="text-3xl text-center font-bold mb-[-32px]">
+          O que você procura?
+        </h2>
         <CategoriesSlider />
       </main>
     </>
