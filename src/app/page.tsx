655ef213bdd69f0aaa79b7ed145bd7f8e8006d33
Murilo Francisco Matos1 <murilofm.social@gmail.com>
2025-05-12 16:34:37 -0300

    Feat: setup for produtos populares

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,16 @@ export default function Home() {
           O que você procura?
         </h2>
         <CategoriesSlider />
-        <BannerNovidades />
+        <div className="w-full order-2 sm:order-1">
+          <BannerNovidades />
+        </div>
+        <div></div>
+        <div className="order-1 sm:order-2">
+          <h2 className="text-3xl w-full font-bold mb-3 mt-7">
+            Produtos Populares
+          </h2>
+          <div className="flex flex-col sm:flex-row gap-6"></div>
+        </div>
       </main>
     </>
   );
