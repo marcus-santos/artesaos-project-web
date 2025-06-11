8de992e87ad3b9773468e54d89c420461db030d9
Aluno-SamuelRSilva <samuel.rodrigues1@aluno.ifsp.edu.br>
2025-06-10 22:14:43 -0300

    feat: add ProductSlide component and integrate into ProductPage for improved product display

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -12,6 +12,7 @@ import ProductReviews from '../components/ProductReviews';
 import { FiPlus } from 'react-icons/fi';
 import {BaseCard, ProductCardBody}  from '@/components/Card';
 import Image from 'next/image';
+import ProductSlide from '../components/ProductSlide';
 
 interface Review {
     reviewer: string;
@@ -151,23 +152,11 @@ function ProductPage() {
                     )}
 
                     <div>
-                        <div className='flex flex-row items-center'>
-                            <div className='flex items-center gap-2 px-8 py-4 text-2xl font-bold'>
-                                <FiPlus/>
-                                <h1>Produtos do Artista</h1>
-                            </div>
-
-                            <div className='hidden md:flex justify-end flex-1 text-xs items-center px-4'>
-                                <button 
-                                    onClick={() => alert('Ver mais produtos do artista!')}
-                                    className="p-1.5 bg-white text-[#1B7132] border-1 border-[#ABCFB5] rounded-md hover:bg-[#ABCFB5] hover:text-white transition-colors"
-                                >
-                                    Ver mais
-                                </button>
-                            </div>
-                        </div>
-
-                        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 md:grid-cols-4 gap-4 p-4">
+                        <ProductSlide 
+                            icon={<FiPlus className="text-2xl" />}
+                            title="Produtos do Artista"
+                            onViewMore={() => alert('Ver mais produtos do artista!')}
+                        >
                             {products.map((product, i) => (
                                 <BaseCard key={product.id || i}>
                                     <div className="relative w-full h-40">
@@ -185,26 +174,14 @@ function ProductPage() {
                                     />
                                 </BaseCard>
                             ))}
-                        </div>
+                        </ProductSlide>
                     </div>
 
                     <div>
-                        <div className='flex flex-row'>
-                            <div className='font-bold text-2xl items-center px-8 py-4'>
-                                <h1>Produtos Relacionados</h1>
-                            </div>
-
-                            <div className='hidden md:flex justify-end flex-1 text-xs items-center px-4'>
-                                <button 
-                                    onClick={() => alert('Ver mais produtos do artista!')}
-                                    className="p-1.5 bg-white text-[#1B7132] border-1 border-[#ABCFB5] rounded-md hover:bg-[#ABCFB5] hover:text-white transition-colors"
-                                >
-                                    Ver mais
-                                </button>
-                            </div>
-                        </div>
-
-                        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 md:grid-cols-4 gap-4 p-4">
+                        <ProductSlide 
+                            title="Produtos Relacionados"
+                            onViewMore={() => alert('Ver mais produtos relacionados!')}
+                        >
                             {products.map((product, i) => (
                                 <BaseCard key={product.id || i}>
                                     <div className="relative w-full h-40">
@@ -222,7 +199,7 @@ function ProductPage() {
                                     />
                                 </BaseCard>
                             ))}
-                        </div>
+                        </ProductSlide>
                     </div>
                 </div>
             </main>
