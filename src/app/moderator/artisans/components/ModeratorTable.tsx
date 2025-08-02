76335ec818f7ef1aecc8f8b2404c097f35230c60
MARCUS VINICIUS DA CRUZ SANTOS <CG3028518@car.ifsp>
2025-08-01 22:10:00 -0300

    feat: table responsivity

--- src/app/moderator/artisans/components/ModeratorTable.tsx
+++ src/app/moderator/artisans/components/ModeratorTable.tsx
@@ -20,8 +20,8 @@ function ModeratorTable() {
         <thead className="bg-baby-blue">
           <tr>
             <th scope="col" className="font-semibold p-2 text-sm rounded-tl-md ring-[0.5px]">Nome</th>
-            <th scope="col" className="font-semibold text-sm ring-[0.5px]">Email</th>
-            <th scope="col" className="font-semibold px-6 text-sm ring-[0.5px]">Status</th>
+            <th scope="col" className="font-semibold text-sm ring-[0.5px] hidden md:table-cell">Email</th>
+            <th scope="col" className="font-semibold px-6 text-sm ring-[0.5px] hidden md:table-cell">Status</th>
             <th scope="col" className="font-semibold text-sm rounded-tr-md ring-[0.5px]">Ações</th>
           </tr>
         </thead>
@@ -32,56 +32,67 @@ function ModeratorTable() {
             if (artisan.status === "PENDING") {
               actionCell = (
                 <div className="flex py-1 justify-center items-center gap-2.5">
-                  <Button className="h-6 text-xs my-1 bg-green-600 cursor-pointer" aria-label="Aprovar artesão">
-                    <FaCheck className="text-white" />
-                    APROVAR
+                  <Button className="h-6 text-xs my-1 bg-green-600 cursor-pointer" aria-label="Aprovar artesão" asChild>
+                    <div><FaCheck className="text-white" />
+                      <p className="hidden md:inline">APROVAR</p>
+                    </div>
                   </Button>
-                  <Button className="h-6 text-xs my-1 bg-yellow-600/75 cursor-pointer" aria-label="Editar artesão">
-                    <LuPencil className="text-white" />
-                    EDITAR
+                  <Button className="h-6 text-xs my-1 bg-yellow-600/75 cursor-pointer" aria-label="Editar artesão" asChild>
+                    <div><LuPencil className="text-white" />
+                      <p className="hidden md:inline">EDITAR</p>
+                    </div>
                   </Button>
                   <Button className="h-6 text-xs my-1 bg-red-700 cursor-pointer" aria-label="Recusar artesão">
-                    <BsXLg className="text-white" />
-                    RECUSAR
+                    <div className="flex items-center gap-2">
+                      <BsXLg className="text-white" />
+                      <p className="hidden md:inline">RECUSAR</p>
+                    </div>
                   </Button>
                 </div>
               );
             } else if (artisan.status === "APPROVED") {
               actionCell = (
                 <div className="flex justify-center items-center gap-2.5">
-                  <Button className="h-6 text-xs my-1 bg-yellow-600/75 cursor-pointer" aria-label="Editar artesão">
-                    <LuPencil className="text-white" />
-                    EDITAR
+                  <Button className="h-6 text-xs my-1 bg-yellow-600/75 cursor-pointer" aria-label="Editar artesão" asChild>
+                    <div><LuPencil className="text-white" />
+                      <p className="hidden md:inline">EDITAR</p>
+                    </div>
                   </Button>
-                  <Button className="h-6 text-xs my-1 bg-white font-bold border border-red-600 text-red-600 cursor-pointer" aria-label="Desativar artesão">
-                    <BsXLg className="text-red-600" />
-                    DESATIVAR
+                  <Button className="h-6 text-xs my-1 md:bg-white font-bold border border-red-600 bg-red-600 text-red-600 cursor-pointer" aria-label="Desativar artesão" asChild>
+                    <div>
+                      <BsXLg className="text-white md:text-red-600" />
+                      <p className="hidden md:inline">DESATIVAR</p>
+                    </div>
                   </Button>
                 </div>
               );
             } else if (artisan.status === "REJECTED") {
               actionCell = (
                 <div className="flex justify-center items-center gap-2.5">
-                  <Button className="h-6 text-xs my-1 bg-green-600 cursor-pointer" aria-label="Aprovar artesão">
-                    <FaCheck className="text-white" />
-                    APROVAR
+                  <Button className="h-6 text-xs my-1 bg-green-600 cursor-pointer" aria-label="Aprovar artesão" asChild>
+                    <div><FaCheck className="text-white" />
+                      <p className="hidden md:inline">APROVAR</p>
+                    </div>
                   </Button>
-                  <Button className="h-6 text-xs my-1 bg-yellow-600/75" aria-label="Editar artesão">
-                    <LuPencil className="text-white" />
-                    EDITAR
+                  <Button className="h-6 text-xs my-1 bg-yellow-600/75 cursor-pointer" aria-label="Editar artesão" asChild>
+                    <div><LuPencil className="text-white" />
+                      <p className="hidden md:inline">EDITAR</p>
+                    </div>
                   </Button>
                 </div>
               );
             } else if (artisan.status === "INACTIVE") {
               actionCell = (
                 <div className="flex justify-center items-center gap-2.5">
-                  <Button className="h-6 text-xs my-1 bg-white border border-green-600 text-green-600 cursor-pointer" aria-label="Aprovar artesão">
-                    <FaCheck className="text-green-600" />
-                    ATIVAR
+                  <Button className="h-6 text-xs my-1 bg-green-600 md:bg-white border border-green-600 text-green-600 cursor-pointer" aria-label="Aprovar artesão" asChild>
+                    <div><FaCheck className="text-white md:text-green-600" />
+                      <p className="hidden md:inline">ATIVAR</p></div>
                   </Button>
-                  <Button className="h-6 text-xs my-1 bg-yellow-600/75 cursor-pointer" aria-label="Editar artesão">
-                    <LuPencil className="text-white" />
-                    EDITAR
+                  <Button className="h-6 text-xs my-1 bg-yellow-600/75 cursor-pointer" aria-label="Editar artesão" asChild>
+                    <div>
+                      <LuPencil className="text-white" />
+                      <p className="hidden md:inline">EDITAR</p>
+                    </div>
                   </Button>
                 </div>
               );
@@ -94,8 +105,8 @@ function ModeratorTable() {
                     {artisan.name}
                   </Link>
                 </td>
-                <td className="ring-[0.5px]">{artisan.email}</td>
-                <td className="font-semibold ring-[0.5px]">{statusTranslated[artisan.status]}</td>
+                <td className="ring-[0.5px] hidden md:table-cell ">{artisan.email}</td>
+                <td className="font-semibold ring-[0.5px] hidden md:table-cell">{statusTranslated[artisan.status]}</td>
                 <td className="ring-[0.5px]">{actionCell}</td>
               </tr>
             );
